4c390c43602ef9b939c0a5e02a84c1ab7ba8e5ee
ParZZ1vaLl <trolacochannel@gmail.com>
2024-10-14 23:09:04 +0300

    completed quiz3!

--- rustlings/exercises/quiz3.rs
+++ rustlings/exercises/quiz3.rs
@@ -9,8 +9,8 @@
 
 // I AM NOT DONE
 
-pub fn times_two(num: i32) -> i32 {
-    num * 2
+pub fn is_even(num: i32) -> bool {
+    num % 2 == 0
 }
 
 #[cfg(test)]
@@ -18,13 +18,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn returns_twice_of_positive_numbers() {
-        assert_eq!(times_two(4), ???);
+    fn is_true_when_even() {
+        assert!(is_even(4)); // Перевіряємо, що 4 є парним числом
     }
 
     #[test]
-    fn returns_twice_of_negative_numbers() {
-        // TODO replace unimplemented!() with an assert for `times_two(-4)`
-        unimplemented!()
+    fn is_false_when_odd() {
+        assert!(!is_even(5)); // Перевіряємо, що 5 є непарним числом
     }
 }
